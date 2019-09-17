d150a4dcf1dd72d8e3771efb81cd7d31682c76b1
Matthew Larrubia <mlarrubia@columbushs.com>
2019-09-17 10:42:04 -0400

    Added Instructions

--- Nursery Rhymes/src/nursery/rhymes/NurseryRhymes.java
+++ Nursery Rhymes/src/nursery/rhymes/NurseryRhymes.java
@@ -15,7 +15,33 @@ public class NurseryRhymes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        farm("cow", "moo");
+        farm("duck", "quack");        
+        // TODO: add another animal to the farm here
+        hickory_dickory(1);
+        hickory_dickory(2);
+        // TODO: make the clock strike three here
+        
+        // TODO: call your new methods here ( you must write them first! ) 
+        
+        
     }
+    
+    
+    
+    public static void farm(String animal, String sound){
+        // TODO: write your code here
+    }
+    
+    public static void monkeys(int number){
+        // TODO: write your code here
+    }
+    
+    public static void hickory_dickory(int time){
+        // TODO: write your code here
+    }
+    
+    // TODO add your new methods here
     
 }
