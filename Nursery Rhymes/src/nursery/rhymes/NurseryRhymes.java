3ca7acd24d6cb3fa3a7c1100b817f63a05bb6f44
Robert Romero <21332@columbushs.com>
2019-09-23 09:38:44 -0400

    nurseryRhymes

--- Nursery Rhymes/src/nursery/rhymes/NurseryRhymes.java
+++ Nursery Rhymes/src/nursery/rhymes/NurseryRhymes.java
@@ -16,8 +16,8 @@ public class NurseryRhymes {
      */
     public static void main(String[] args) {
         
-        farm("cow", "moo");
-        farm("duck", "quack");        
+        //farm("cow", "moo");
+        //farm("duck", "quack");        
         // TODO: add another animal to the farm here
         hickory_dickory(1);
         hickory_dickory(2);
@@ -30,18 +30,53 @@ public class NurseryRhymes {
     
     
     
+    
+    
+    
+    
     public static void farm(String animal, String sound){
-        // TODO: write your code here
+        System.out.println("Old MacDonald had a farm, E-I-E-I-O" );
+        System.out.println("and on his farm he had a " + animal + ", E-I-E-I-O");
+        System.out.println("With a " + sound + "-" + sound + "here and a " + sound + "-" + sound + "there");
+        System.out.println("Here a " + sound );
+        System.out.println("There a " + sound );
+        System.out.println("Everywhere" + sound + "-" + sound);
+        System.out.println("Old MacDonald had a farm, E-I-E-I-O" );
     }
     
     public static void monkeys(int number){
-        // TODO: write your code here
+        
+        while (number > 0){
+          System.out.println(number + " little mokeys jumping on the bed");
+          System.out.println( "One fell off and bumped his head");
+          System.out.println( "Mama called the doctor, the doctor said");
+          System.out.println( "\"No more mokeys jumping on the bed\"");
+          number -= 1;
+        }
     }
     
     public static void hickory_dickory(int time){
-        // TODO: write your code here
+        while (time < 13){
+            System.out.println("Hickory dickory dock");
+            System.out.println("The mouse ran up the clock");
+            System.out.println("The clock struck" + time);
+            System.out.println("The mouse ran down");
+            System.out.println("Hickory dickory dock");
+            time += 1;
+        }
     }
     
     // TODO add your new methods here
     
+    public static void milk(int a){
+    
+        while (a > 0){
+          System.out.println(a + "bottles of milk on the wall");
+          System.out.println(a + "bottles of milk");
+          System.out.println("Take one down, pass it around"); 
+          a -= 1;
+          
+        }
+    }
 }
+    
